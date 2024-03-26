74f175dbacaa3cc31480db1ae0fd33d722b7dd57
Erudite-pratham <pratham.consultadd@gmail.com>
2024-03-26 15:50:24 -0400

    Default Project

---
+++ src/main/java/com/example/Capstone/Project2/CapstoneProject2Application.java
@@ -0,0 +1,13 @@
+package com.example.Capstone.Project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Capstone/Project2/CapstoneProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Capstone.Project2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
