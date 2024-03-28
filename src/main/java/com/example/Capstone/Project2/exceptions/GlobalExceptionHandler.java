c43fe71b0eb6e7b38848cb11ef0e995c3124d38c
Erudite-pratham <pratham.consultadd@gmail.com>
2024-03-28 15:29:40 -0400

    GlobalExceptionHandler.java created

---
+++ src/main/java/com/example/Capstone/Project2/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.example.Capstone.Project2.exceptions;
+
+import com.example.Capstone.Project2.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGlobalException(Exception ex, WebRequest request) {
+        String errorMessage = ex.getMessage();
+        ApiResponse response = new ApiResponse(errorMessage, false, null);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+        ApiResponse response = new ApiResponse("User not found", false, null);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<Object> handleUserAlreadyExistsException(UserAlreadyExistsException ex, WebRequest request) {
+        ApiResponse response = new ApiResponse("User already exists", false, null);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+}
